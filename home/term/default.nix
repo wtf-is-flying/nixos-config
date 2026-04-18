9fad00a895793409703cbb9d240ef9c72c6a989f
wtf-is-flying <106453261+wtf-is-flying@users.noreply.github.com>
2026-04-18 17:50:59 +0200

    add ghostty

--- home/term/default.nix
+++ home/term/default.nix
@@ -3,4 +3,15 @@
     enable = true;
     extraConfig = builtins.readFile ./wezterm.lua;
   };
+
+  programs.ghostty = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      command = "fish";
+      font-family = "JetBrainsMono Nerd Font";
+      font-size = 10;
+      theme = "Everforest Dark Hard";
+    };
+  };
 }
