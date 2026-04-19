9cd65d696823d4e680761d542256a9b64f8155c2
wtf-is-flying <106453261+wtf-is-flying@users.noreply.github.com>
2026-04-19 09:45:32 +0200

    set default browser

--- home/default.nix
+++ home/default.nix
@@ -52,4 +52,21 @@
 
     vesktop.enable = true;
   };
+
+  # Default applications
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications =
+      let
+        librewolf = "librewolf.desktop";
+      in
+      {
+        "text/html" = librewolf;
+        "x-scheme-handler/http" = librewolf;
+        "x-scheme-handler/https" = librewolf;
+        "x-scheme-handler/about" = librewolf;
+        "x-scheme-handler/unknown" = librewolf;
+      };
+  };
 }
