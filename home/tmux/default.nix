886118b46c71b9321bec30b145a379fe9c9103c5
wtf-is-flying <106453261+wtf-is-flying@users.noreply.github.com>
2026-05-02 18:38:06 +0200

    cleanup tmux config

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -11,17 +11,6 @@
       tmuxPlugins.sensible
       tmuxPlugins.vim-tmux-navigator
       tmuxPlugins.yank
-      {
-        plugin = tmuxPlugins.resurrect;
-        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-      }
-      {
-        plugin = tmuxPlugins.continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '60' # minutes
-        '';
-      }
     ];
     extraConfig = builtins.readFile ./tmux.conf;
   };
