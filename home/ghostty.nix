267e1b3e44bd25045d65f91eabfeb99898585f00
wtf-is-flying <106453261+wtf-is-flying@users.noreply.github.com>
2026-05-28 11:09:42 +0200

    fix ghostty for darwin

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -1,13 +1,16 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  inherit (pkgs.stdenv) isLinux;
+in
 {
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
-    package = if pkgs.stdenv.isLinux then pkgs.ghostty else pkgs.ghostty-bin;
+    package = if isLinux then pkgs.ghostty else pkgs.ghostty-bin;
     settings = {
-      command = "fish";
+      command = lib.getExe pkgs.fish;
       font-family = "JetBrainsMono Nerd Font";
-      font-size = 10;
+      font-size = if isLinux then 10 else 12;
       theme = "Vague";
     };
   };
